aed3db2b9590ddef881c87f58d841ad8502f8b72
daniel <daniel@outboss.io>
2021-06-24 17:03:17 -0400

    add home

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib import admin
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.HomeView.as_view(), name='home'),
 ]
